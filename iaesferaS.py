ff27e6d8af445507f3f149e906550e908c59aa47
ramrals <128934271+ramrals@users.noreply.github.com>
2024-01-03 17:37:03 -0300

    17:36:42 4 esferas
    variante

---
+++ iaesferaS.py
@@ -0,0 +1,92 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.spatial import Delaunay
+
+# Generar una esfera utilizando ecuaciones paramétricas
+u = np.linspace(0, 2 * np.pi, 21)
+v = np.linspace(0, np.pi, 31)
+x = np.outer(np.cos(u), np.sin(v))
+y = np.outer(np.sin(u), np.sin(v))
+z = np.outer(np.ones(np.size(u)), np.cos(v))
+
+# Generar una segunda esfera atravesada
+u2 = np.linspace(0, 2 * np.pi, 21)
+v2 = np.linspace(0, np.pi, 31)
+x2 = np.outer(np.cos(u2), np.sin(v2))
+y2 = np.outer(np.sin(u2), np.sin(v2))
+z2 = np.outer(np.ones(np.size(u2)), np.cos(v2))
+z2 = z2 - 1
+
+# Generar una tercera esfera atravesada
+u3 = np.linspace(0, 2 * np.pi, 21)
+v3 = np.linspace(0, np.pi, 31)
+x3 = np.outer(np.cos(u3), np.sin(v3))
+y3 = np.outer(np.sin(u3), np.sin(v3))
+z3 = np.outer(np.ones(np.size(u3)), np.cos(v3))
+z3 = z3 - 2
+
+# Generar una cuarta esfera atravesada
+u4 = np.linspace(0, 2 * np.pi, 21)
+v4 = np.linspace(0, np.pi, 31)
+x4 = np.outer(np.cos(u4), np.sin(v4))
+y4 = np.outer(np.sin(u4), np.sin(v4))
+z4 = np.outer(np.ones(np.size(u4)), np.cos(v4))
+z4 = z4 - 3
+
+# Crear una figura y un eje 3D
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+# Encontrar los nodos de ambas esferas
+nodos = np.array([x.flatten(), y.flatten(), z.flatten()]).T
+nodos2 = np.array([x2.flatten(), y2.flatten(), z2.flatten()]).T
+nodos3 = np.array([x3.flatten(), y3.flatten(), z3.flatten()]).T
+nodos4 = np.array([x4.flatten(), y4.flatten(), z4.flatten()]).T
+
+# Crear una malla para conectar los nodos
+nodos_combinados = np.concatenate((nodos, nodos2, nodos3, nodos4))
+malla = Delaunay(nodos_combinados)
+
+# Crear nodos de entrada y salida
+nodo_entrada = nodos_combinados[0, :]
+nodo_salida = nodos_combinados[-1, :]
+
+# Dibujar los nodos y la malla
+ax.scatter(nodos[:, 0], nodos[:, 1], nodos[:, 2], color='red')
+ax.scatter(nodos2[:, 0], nodos2[:, 1], nodos2[:, 2], color='blue')
+ax.scatter(nodos3[:, 0], nodos3[:, 1], nodos3[:, 2], color='green')
+ax.scatter(nodos4[:, 0], nodos4[:, 1], nodos4[:, 2], color='yellow')
+
+# Dibujar las conexiones en violeta
+for simplex in malla.simplices:
+    ax.plot3D([nodos_combinados[simplex[0], 0], nodos_combinados[simplex[1], 0], nodos_combinados[simplex[2], 0]],
+              [nodos_combinados[simplex[0], 1], nodos_combinados[simplex[1], 1], nodos_combinados[simplex[2], 1]],
+              [nodos_combinados[simplex[0], 2], nodos_combinados[simplex[1], 2], nodos_combinados[simplex[2], 2]],
+              color='violet', linewidth=3)
+
+# Dibujar el nodo de entrada y el nodo de salida
+ax.scatter(nodo_entrada[0], nodo_entrada[1], nodo_entrada[2], color='green', marker='o', s=100)
+ax.scatter(nodo_salida[0], nodo_salida[1], nodo_salida[2], color='yellow', marker='o', s=100)
+
+# Añadir función de activación tanh
+def tanh(x):
+    return np.tanh(x)
+
+for simplex in malla.simplices:
+    ax.plot3D([tanh(nodos_combinados[simplex[0], 0]), tanh(nodos_combinados[simplex[1], 0]), tanh(nodos_combinados[simplex[2], 0])],
+              [tanh(nodos_combinados[simplex[0], 1]), tanh(nodos_combinados[simplex[1], 1]), tanh(nodos_combinados[simplex[2], 1])],
+              [tanh(nodos_combinados[simplex[0], 2]), tanh(nodos_combinados[simplex[1], 2]), tanh(nodos_combinados[simplex[2], 2])],
+              color='violet', linewidth=3, alpha=0.5)
+
+# Establecer los límites del eje
+ax.set_xlim([-1.1, 1.1])
+ax.set_ylim([-1.1, 1.1])
+ax.set_zlim([-2.1, 1.1])
+
+# Añadir etiquetas a los ejes
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+
+# Mostrar la trama
+plt.show()
